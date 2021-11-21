d861a88fe166c51a2ebf9fc2bf553256e7bd12be
Michael Redig <mredig@gmail.com>
2021-11-20 19:39:34 -0600

    updated health checks

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -5,10 +5,20 @@ func routes(_ app: Application) throws {
         return "It works!"
     }
 
-	app.get("_ah", "health") { request -> String in
-		print("All - /_ah/health route handler...")
+	app.get("readiness_check") { request -> String in
+		print("All - /readiness_check handler...")
 		return "OK"
 	}
+
+	app.get("liveness_check") { request -> String in
+		print("All - /liveness_check route handler...")
+		return "OK"
+	}
+
+//	app.get("_ah", "health") { request -> String in
+//		print("All - /_ah/health route handler...")
+//		return "OK"
+//	}
 
 	app.get("hello") { request -> String in
 		print("GET - /hello route handler...")
